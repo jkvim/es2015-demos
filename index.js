cda3d23dec8b62a2375024fd4dda2d9469a3f3c8
xulianghui1 <twomeetings@gmail.com>
2017-06-16 20:01:52 +0800

    scope

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const start ='it works'
 console.log('--------', start)
+
+var a = 100
+if(a % 2 === 0){
+	let result = '能被2整除'
+	console.log('in:', result)
+}
+console.log('out:', result)
+
+ for(var i=0; i<10; i++){
+		console.log('i:',i)
+ }
+
+ console.log(i)
