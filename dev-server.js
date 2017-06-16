ec4752dba98f2ea44158a202ec23219b5ca0fbbb
Chuan.Peng <cpeng@thoughtworks.com>
2017-06-16 19:23:49 +0800

    Add webpack-dev-server.

---
+++ dev-server.js
@@ -0,0 +1,15 @@
+const Webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const webpackConfig = require("./webpack.config");
+
+const compiler = Webpack(webpackConfig);
+const server = new WebpackDevServer(compiler, {
+    compress: true,
+    stats: {
+        colors: true
+    }
+});
+
+server.listen(8080, "127.0.0.1", function () {
+    console.log("Starting server on http://localhost:8080");
+});
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 require('./index.css');
 
 document.querySelector('#root').innerText = 'Hello, I\'m from JS.';
+
+console.log(213)
