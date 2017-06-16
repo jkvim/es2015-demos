f59941877006675e77b3bfbbaceabc5641eb575a
jkvim <jkvim@outlook.com>
2017-06-16 19:35:45 +0800

    Add post form router.

--- mock_server/server.js
+++ mock_server/server.js
@@ -6,6 +6,15 @@ server.use('/hello', (req, res) => {
     res.end();
 });
 
+server.use(jsonServer.bodyParser);
+server.use('/form', (req, res) => {
+    if (req.method === 'POST') {
+        const { name, email } = req.body
+        res.jsonp(`Hi ${name}, your email is ${email}.`)
+    }
+    res.end();
+})
+
 server.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
