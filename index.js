812ed4addf36952ba7a0a6ea4086d6a9f194bbfb
jkvim <jkvim@outlook.com>
2017-06-20 15:31:15 +0800

    Init react-demo6.

--- index.js
+++ index.js
@@ -2,22 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import style from './index.scss'
 
-const Button = ({ type, text }) => {
-    const className = type === 'alert' ? style.alertButton : style.mainButton
-    return (
-        <button className={`${className} ${style.button}`}>{text}</button>
-    );
-}
-
-const MainButton = () => <Button type="submit" text="Submit" />
-const AlertButton = () => <Button type="alert" text="Alert" />
-
 class App extends React.Component {
     render() {
         return (
             <div>
-                <MainButton />
-                <AlertButton />
+                Hello World
             </div> 
         );
     }
