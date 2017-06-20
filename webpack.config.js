cd5b8f3e25c15261fef02224ddc6540dd7c439b1
Chuan.Peng <cpeng@thoughtworks.com>
2017-06-20 14:14:51 +0800

    Remove hot-load.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const absolutePath = (relativePath) => {
-    return path.resolve(__dirname, relativePath);
-};
 
 module.exports = {
     entry: {
-        app: absolutePath('./index.js')
+        app: path.resolve(__dirname, './index.js')
     },
     output: {
+        publicPath: '/',
         filename: '[name].js'
-    },
-    devServer: {
-        hot: true,
-        port: 8080,
-        inline: true,
-        host: '0.0.0.0',
-        compress: true,
-        publicPath: '/'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            filename: 'index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin()
-    ]
+    }
 };
