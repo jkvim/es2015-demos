dc06562083281b48555cec95de50b987187959de
jkvim <jkvim@outlook.com>
2017-06-20 16:13:59 +0800

    Init react-demo7.

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import style from './index.scss'
 
-import Counter from './components/Counter';
-
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Counter />
-            </div> 
+                Hello Wolrd
+            </div>
         );
     }
 }
