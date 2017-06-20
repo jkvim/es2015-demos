844c48da5948fb492f55786af7c848a5e6af09f3
jkvim <jkvim@outlook.com>
2017-06-20 15:32:34 +0800

    Add counter, react-demo6 completed.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import style from './index.scss'
 
+import Counter from './components/Counter';
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                Hello World
+                <Counter />
             </div> 
         );
     }
