77fe34550b3777cd9fcbf07c457f51632840b5ba
Chuan.Peng <cpeng@thoughtworks.com>
2017-06-20 16:42:47 +0800

    Remove exclude directory.

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,11 @@ module.exports = {
         app: path.resolve(__dirname, './index.js')
     },
     output: {
-        publicPath: '/',
         filename: '[name].js'
     },
     module: {
         rules: [{
             test: /\.scss$/,
-            exclude: /node_modules/,
             use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
         }, {
             test: /\.js$/,
