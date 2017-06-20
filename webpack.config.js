20df67a4018f692d9d682230c4da336c97f2c551
Chuan.Peng <cpeng@thoughtworks.com>
2017-06-20 14:51:51 +0800

    Remove commonsChunkPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,43 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const absolutePath = (relativePath) => {
-    return path.resolve(__dirname, relativePath);
-};
-
 module.exports = {
     entry: {
-        app: absolutePath('./index.js')
+        app: path.resolve(__dirname, './index.js')
     },
     output: {
+        publicPath: '/',
         filename: '[name].js'
-    },
-    devServer: {
-        hot: true,
-        port: 8080,
-        inline: true,
-        host: '0.0.0.0',
-        compress: true,
-        publicPath: '/'
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
             filename: 'index.html'
         }),
-        new webpack.HotModuleReplacementPlugin(),
         new ExtractTextPlugin({
             filename: '[name].css'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            names: ['vendor']
         })
     ],
     module: {
         rules: [{
             test: /\.scss$/,
+            exclude: /node_modules/,
             use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [{
-                    loader: 'css-loader'
-                }, {
-                    loader: 'postcss-loader'
-                }, {
-                    loader: 'sass-loader'
-                }]
+                use: ['css-loader', 'postcss-loader', 'sass-loader']
             })
         }, {
             test: /\.js$/,
-            use: [{
-                loader: 'babel-loader'
-            }]
+            exclude: /node_modules/,
+            use: ['babel-loader']
         }, {
             test: /\.(jpg|png|gif)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 20480
-                }
-            }]
+            exclude: /node_modules/,
+            use: ['url-loader']
         }, {
             test: /\.(eot|svg|ttf|woff)$/,
-            use: [{
-                loader: 'file-loader'
-            }]
+            exclude: /node_modules/,
+            use: ['file-loader']
         }]
     }
 };
