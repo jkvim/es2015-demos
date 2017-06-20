1dd99b8765e644b8f26bea42ad2739877d8b2891
jkvim <jkvim@outlook.com>
2017-06-20 16:41:31 +0800

    react-demo7 completed.

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import style from './index.scss'
 
+import ControlledForm from './components/ControlledForm';
+import UnControlledForm from './components/UnControlledForm';
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                Hello Wolrd
+                <h1>受控组件</h1>
+                <ControlledForm />
+                <h1>非受控组件</h1>
+                <UnControlledForm />
             </div>
         );
     }
